938b6c509f8681eb9ce90865503d37977ec514ec
ZebGirouard <zeb.girouard@generalassemb.ly>
2019-07-31 16:35:11 -0600

    Halfway done

---
+++ temperatures_api/app/controllers/locations_controller.rb
@@ -0,0 +1,21 @@
+class LocationsController < ApplicationController
+  before_action :set_location, only: [:show]
+
+  # GET /locations
+  def index
+    @locations = Location.all
+
+    render json: @locations
+  end
+
+  # GET /locations/1
+  def show
+    render json: @location.to_json(include: :temperatures)
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_location
+      @location = Location.find(params[:id])
+    end
+end
